e1c5b1489032e8d5701f58c39e20e45aef425282
Ana-Gabs <2022371069@uteq.edu.mx>
2025-03-31 16:01:10 -0600

    Verificar coneción con firebase

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./src/routes/user_routes");
+const { db } = require("./src/config/firebase"); 
 
-const app = express();
+const app = express();  
 
 // variables de entorno
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 const IP_WEBSERVICE_URL = process.env.IP_WEBSERVICE_URL;
 
 // Middlewares
@@ -16,6 +17,15 @@ app.use(cors());
 
 // rutas
 app.use("/users", userRoutes);
+
+// Verificar conexión con Firestore antes de iniciar el servidor
+db.listCollections()
+  .then(() => {
+    console.log("Conexión con Firestore establecida correctamente.");
+  })
+  .catch((error) => {
+    console.error("Error al conectar con Firestore:", error);
+  });
 
 // arranque del servidor
 app.listen(PORT, () => {
