db893f2872b1478ca66f9ee8bb8e21cdfd91d4ca
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-01 15:07:17 -0600

    "sin rate limit"

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -2,16 +2,15 @@
 const express = require("express");
 const { login, register, getInfo, verifyOtp } = require("../controllers/user_controller");
 const verifyToken = require("../middlewares/authMiddleware");
-const limiter = require("../middlewares/rateLimitMiddleware");
 
 const router = express.Router();
 
 // Rutas de autenticación
-router.post("/login", limiter, login);
-router.post("/register", limiter, register);
+router.post("/login", login);
+router.post("/register", register);
 router.get("/info", getInfo);
 //router.get("/info", verifyToken, getInfo);
-router.post("/verify-otp", limiter, verifyOtp);
+router.post("/verify-otp", verifyOtp);
 
 
 module.exports = router;
