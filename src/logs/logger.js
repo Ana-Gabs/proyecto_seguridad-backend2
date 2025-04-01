a2df00722a42032490ffadaa7583f4c777ef0623
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-01 11:37:47 -0600

    "crea la carpeta logs si no exixste"

--- src/logs/logger.js
+++ src/logs/logger.js
@@ -1,15 +1,21 @@
+// ./logs/logger.js
 const winston = require('winston');
 const path = require('path');
+const fs = require('fs');
 
 const logDirectory = path.join(__dirname, '../../logs');
+
+if (!fs.existsSync(logDirectory)) {
+  fs.mkdirSync(logDirectory, { recursive: true });
+}
 
 const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: `${logDirectory}/error.log`, level: 'error' }),
-    new winston.transports.File({ filename: `${logDirectory}/combined.log` }),
-    new winston.transports.File({ filename: `${logDirectory}/all.log`, level: 'info' }), // 👈 Agregamos all.log
+    new winston.transports.File({ filename: path.join(logDirectory, 'error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.join(logDirectory, 'combined.log') }),
+    new winston.transports.File({ filename: path.join(logDirectory, 'all.log'), level: 'info' }),
   ],
 });
 
