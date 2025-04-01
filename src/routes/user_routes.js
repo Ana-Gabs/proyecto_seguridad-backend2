06eef8f6df130cc795c74e0ab324548794e4b327
Ana-Gabs <2022371069@uteq.edu.mx>
2025-04-01 11:31:38 -0600

    "routes usuarios"

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,14 +1,17 @@
 // ./routes/user_routes.js
 const express = require("express");
-const { getInfo, register, login, verifyOtp } = require("../controllers/user_controller");  
+const { login, register, getInfo, verifyOtp } = require("../controllers/user_controller");
+const verifyToken = require("../middlewares/authMiddleware");
+const limiter = require("../middlewares/rateLimitMiddleware");
 
 const router = express.Router();
 
 // Rutas de autenticación
-router.get("/getInfo", getInfo);
-router.post("/register", register);
-router.post("/login", login);
+router.post("/login", limiter, login);
+router.post("/register", limiter, register);
+router.get("/info", verifyToken, getInfo);
 router.post("/verify-otp", verifyOtp);
 
 module.exports = router;
+
 
